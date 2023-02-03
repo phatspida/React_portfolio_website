d402d73ebb0337208511df925caf9d159fdbe402
phatspida <ahmedmomodu97@gmail.com>
2023-02-03 11:23:23 +0100

    Moved whatsaap icon to third position

--- my-portfolio/src/components/header/HeaderSocials.jsx
+++ my-portfolio/src/components/header/HeaderSocials.jsx
@@ -9,9 +9,8 @@ const HeaderSocials = () => {
     <div className="header__socials">
         <a href="https://linkdln.com" target="_blank"><FaLinkedin/></a>
         <a href="https://github.com" target="_blank"><BsGithub /></a>
-        <a href="https://dribble.com" target="_blank"><FiDribbble /></a>
         <a href="https://wa.me/message/HUFPV32Y3QETF1.com" target="_blank"><BsWhatsapp /></a>
-      
+        <a href="https://dribble.com" target="_blank"><FiDribbble /></a>
     </div>
   )
 }
