9075f7db8de2500788ef2a42aab625ac167ba41b
phatspida <ahmedmomodu97@gmail.com>
2023-02-19 14:41:15 +0100

    Navigator changes

--- my-portfolio/src/components/nav/Nav.jsx
+++ my-portfolio/src/components/nav/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
       <a href="#" onClick = {() => setActiveNav('#')} className={activeNav === '#'? 'active' : ''}><AiOutlineHome /></a>
       <a href="#about"  onClick ={() => setActiveNav('#about')} className={activeNav === '#about'? 'active' : ''}><AiOutlineUser /></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience'? 'active' : ''}><RiBookmark3Line /></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services'? 'active' : ''}><FaHandsHelping /></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#services')} className={activeNav === '#services'? 'active' : ''}><FaHandsHelping /></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact'? 'active' : ''}><MdOutlineWifiCalling3 /></a>
 
     </nav>
