e0f3ed1f29d3db2883b4cf0a336d2f00d5ac2906
phatspida <ahmedmomodu97@gmail.com>
2023-05-04 19:10:33 +0100

    Adjusted Specialty

--- my-portfolio/src/components/header/Header.jsx
+++ my-portfolio/src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const header = () => {
         <div className="container header__container">
           <h5>Hello I'm</h5>
           <h1>Ahmed Momodu</h1>
-          <h5 className="text_light">Fullstack Developer</h5>
+          <h5 className="text_light">Fullstack Python Developer</h5>
           <CTA />
           <HeaderSocials />
 
